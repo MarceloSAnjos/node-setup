84c28c7746a38f2b000a62ca9514ea2042db5384
marceloSAnjos <cellodosanjos03@gmail.com>
2020-06-22 23:24:28 -0300

    eslint e jest

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-
-const app = express();
+import express from 'express'
+import '@controllers/UserController'
+const app = express()
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+  return response.json({ message: 'Hello World' })
 })
 
 app.listen(3333)
